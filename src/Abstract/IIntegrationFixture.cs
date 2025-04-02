4db9eebd2bc40515bcaaceac66d7d4c662913771
Jake Soenneker <jake@soenneker.com>
2025-04-01 19:40:42 -0500

    interface def

--- src/Abstract/IIntegrationFixture.cs
+++ src/Abstract/IIntegrationFixture.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Bogus;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Soenneker.Utils.AutoBogus;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Soenneker.Fixtures.Integration.Abstract;
@@ -9,7 +13,39 @@ namespace Soenneker.Fixtures.Integration.Abstract;
 /// </summary>
 public interface IIntegrationFixture : IAsyncLifetime
 {
+    /// <summary>
+    /// A dictionary of registered <see cref="WebApplicationFactory{TStartup}"/> instances by startup type.
+    /// </summary>
+    Dictionary<Type, object> Factories { get; }
+
+    /// <summary>
+    /// A configured instance of <see cref="Faker"/> for generating random data in tests.
+    /// </summary>
+    Faker Faker { get; }
+
+    /// <summary>
+    /// A configured instance of <see cref="AutoFaker"/> using optional custom configuration.
+    /// </summary>
+    AutoFaker AutoFaker { get; }
+
+    /// <summary>
+    /// Initializes the integration fixture with any needed setup logic, such as Faker configuration.
+    /// </summary>
+    /// <returns>A <see cref="ValueTask"/> that completes once the fixture is initialized.</returns>
+    new ValueTask InitializeAsync();
+
+    /// <summary>
+    /// Registers a lazy <see cref="WebApplicationFactory{TStartup}"/> for the specified startup type and project name.
+    /// </summary>
+    /// <typeparam name="TStartup">The startup class of the application under test.</typeparam>
+    /// <param name="projectName">The name of the test project containing the appsettings.json file.</param>
     void RegisterFactory<TStartup>(string projectName) where TStartup : class;
 
+    /// <summary>
+    /// Gets a registered lazy <see cref="WebApplicationFactory{TStartup}"/> for the specified startup type.
+    /// </summary>
+    /// <typeparam name="TStartup">The startup class of the application under test.</typeparam>
+    /// <returns>The registered <see cref="Lazy{T}"/> factory.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if the factory was not registered first.</exception>
     Lazy<WebApplicationFactory<TStartup>> GetFactory<TStartup>() where TStartup : class;
 }
